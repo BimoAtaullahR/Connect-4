38c4a473b365e62b2a15000d07da508f3f5be2b6
bimo <bimoataullahrahman38@gmail.com>
2025-09-28 15:28:28 +0700

    membuat animasi token, menyorot token kemenangan dan membuat sistem ronde

--- connectFour.cpp
+++ connectFour.cpp
@@ -3,15 +3,23 @@
 #include <string>
 #include <algorithm>
 #include <limits>
+#include <chrono>
+#include <thread>
+#include <vector>
 
 #define fi first
 #define se second
+#define pb push_back
 using namespace std;
 
+string pemain1, pemain2;
 char kotak[10][10];
 string arahConnectFour;
-int kolom;
+int kolom, ronde, temp, giliran, giliranTemp, skorPemain1, skorPemain2;
+char token;
 int bawahKolom[10];
+vector<pair<int,int>> tokenKemenangan;
+
 string bgColor = "\033[0m", fontColor = "\033[0m", RESET = "\033[0m";
 string warna;
 pair<string,string> warnaPemain1, warnaPemain2;
@@ -24,7 +32,7 @@ pair<string,string> cyan = {"\033[46m", "\033[1m"};
 void inputWarna(){
     //PEMAIN 1
     bool lanjut = true;
-    cout << "(Pemain 1) Pilih warna favoritmu (merah/hijau/biru/cyan): ";
+    cout << "(" << pemain1 << ") Pilih warna favoritmu (merah/hijau/biru/cyan): ";
     while(lanjut){
         cin >> warna;
         lanjut = false;
@@ -54,7 +62,7 @@ void inputWarna(){
 
     //PEMAIN 2
     lanjut = true;
-    cout << "(Pemain 2) Pilih warna favoritmu (merah/hijau/biru/cyan): ";
+    cout << "(" << pemain2 <<") Pilih warna favoritmu (merah/hijau/biru/cyan): ";
     while(lanjut){
         cin >> warna;
         lanjut = false;
@@ -96,14 +104,21 @@ void cekInput(){
 
 bool connect4(int x, int y, char token){
     //CEK HORIZONTAL
+    tokenKemenangan = {};
     int horizontal = 0;
     for(int i = y; i < 9; i++){
         if(kotak[x][i] != token) break;
-        else horizontal++;
+        else{
+            horizontal++;
+            tokenKemenangan.pb({x, i});
+        } 
     }
     for(int i = y-1; i > 1; i--){
         if(kotak[x][i] != token) break;
-        else horizontal++;
+        else{
+            horizontal++;
+            tokenKemenangan.pb({x,i});
+        } 
     }
     if(horizontal==4){
         arahConnectFour = "Horizontal";
@@ -111,14 +126,20 @@ bool connect4(int x, int y, char token){
     }
 
     //CEK VERTIKAL
-    int vertikal = 0;
+    int vertikal = 0; tokenKemenangan = {};
     for(int i = x; i < 7; i++){
         if(kotak[i][y] != token) break;
-        else vertikal++;
+        else{
+            vertikal++;
+            tokenKemenangan.pb({i, y});  
+        } 
     }
     for(int i = x-1; i > 0; i--){
         if(kotak[i][y] != token) break;
-        else vertikal++;
+        else{
+            vertikal++;
+            tokenKemenangan.pb({i, y});  
+        } 
     }
     if(vertikal == 4){
         arahConnectFour = "Vertikal";
@@ -127,10 +148,11 @@ bool connect4(int x, int y, char token){
 
     int tempX = x, tempY = y;
     //CEK DIAGONAL KIRI ATAS KE KANAN BAWAH
-    int diagonal1 = 0;
+    int diagonal1 = 0; tokenKemenangan = {};
     while(tempX < 7 && tempY < 9){
         if(kotak[tempX][tempY] != token) break;
         diagonal1++;
+        tokenKemenangan.pb({tempX, tempY});  
         tempX++; tempY++;
     }
 
@@ -138,7 +160,9 @@ bool connect4(int x, int y, char token){
     while(tempX > 0 && tempY > 1){
         if(kotak[tempX][tempY] != token) break;
         diagonal1++;
+        tokenKemenangan.pb({tempX, tempY});  
         tempX--; tempY--;
+
     }
     if(diagonal1 == 4){
         arahConnectFour = "Diagonal dari Kiri Atas ke Kanan Bawah";
@@ -147,16 +171,18 @@ bool connect4(int x, int y, char token){
 
     //CEK DIAGONAL KIRI BAWAH KE KANAN ATAS
     tempX = x, tempY = y;
-    int diagonal2 = 0;
+    int diagonal2 = 0; tokenKemenangan = {};
     while(tempX > 0 && tempY < 9){
         if(kotak[tempX][tempY] != token) break;
         diagonal2++;
+        tokenKemenangan.pb({tempX, tempY});  
         tempX--; tempY++;
     }
     tempX = x+1, tempY = y-1;
     while(tempX < 7 && tempY > 1){
         if(kotak[tempX][tempY] != token) break;
         diagonal2++;
+        tokenKemenangan.pb({tempX, tempY});  
         tempX++; tempY--;
     }
     if(diagonal2 == 4){
@@ -164,82 +190,180 @@ bool connect4(int x, int y, char token){
         return true;
     }
 
-    // cout << horizontal << " " << vertikal << " " << diagonal1 << " " << diagonal2 << endl;
     //sampai sini jika tidak ada connect four
     return false;
 }
 
-void cetakKotak(){
+void cetakAnimasiKotak(){
+    int row = 1;
+    while(row <= 6-bawahKolom[kolom+1]+1){
+        system("cls");
+        for(int i = 1; i <= 7; i++){
+            for(int j = 1; j <= 9; j++){
+                    if(i==row && kolom+1==j){
+                        cout << token << " ";
+                    }
+                    else cout <<  kotak[i][j] << " ";
+                }
+            cout << endl;
+        }
+        row++;
+        this_thread::sleep_for(chrono::milliseconds(250));
+    }
+    kotak[6-bawahKolom[kolom+1]+1][kolom+1] = token;
+}
+
+void cetakKotakBiasaDanSkor(){
+    cout << "Ronde "<< ronde - temp + 1 << " (dari " << ronde << ")" << endl;
+    cout << "Skor " << pemain1 << ": " << skorPemain1 << endl;
+    cout << "Skor " << pemain2 << ": " << skorPemain2 << endl;
+    cout << endl;
     for(int i = 1; i <= 7; i++){
         for(int j = 1; j <= 9; j++){
                 cout <<  kotak[i][j] << " ";
             }
         cout << endl;
     }
+    cout << endl;
+}
+
+void cetakKotakKemenangan(){
+    string fontMenang = "\033[1m", bgMenang = "\033[43m";
+    for(int i = 1; i <= 7; i++){
+            for(int j = 1; j <= 9; j++){
+                bool cek = false;
+                    for(pair<int,int> k : tokenKemenangan){
+                        if(k.fi==i && k.se==j){
+                            cek = true;
+                            cout << RESET << bgMenang << fontMenang << kotak[i][j];
+                            // cout << i << " " << j;
+                            cout << RESET << bgColor << fontColor << " ";
+                            break;
+                        }
+                    }
+                    if(!cek) cout <<  kotak[i][j] << " ";
+                }
+            cout << endl;
+        }
+}
+
+void pergantianGiliran(){
+    if(giliranTemp==2){
+        token = 'X';
+        bgColor = warnaPemain2.fi;
+        fontColor = warnaPemain2.se;
+    } 
+    else{
+        token = 'O';
+        bgColor = warnaPemain1.fi;
+        fontColor = warnaPemain1.se;
+    }
 }
 
 int main(){
+    cout << "Nama Pemain 1: "; cin >> pemain1;
+    cout << "Nama Pemain 2: "; cin >> pemain2;
+    cout << "Berapa banyak ronde permainan yang mau dimainkan? (bilangan ganjil): ";
+    while(!(cin >> ronde) || ronde%2==0){
+        cout << "Input tidak valid, silakan input kembali (integer/bilangan ganjil): ";
+    }
+
     inputWarna();
-    bgColor = warnaPemain1.fi; fontColor = warnaPemain1.se;
-    cout << bgColor << fontColor;
-    system("cls");
-    for(int i = 1; i <= 7; i++){
-        for(int j = 1; j <= 9; j++){
-            if(j == 1 || j == 9){
-                kotak[i][j] = '|';
-            }
-            else if(i==7){
-                kotak[i][j] = '-';
-            }
-            else kotak[i][j] = ' ';
-            cout << kotak[i][j] << " ";
-        }
-        cout << endl;
-    }
+    temp = ronde;
+    giliran = 2;
 
-    int round = 1;
-    fill(begin(bawahKolom), end(bawahKolom), 1);
-
-    while(round <= 42){
-        char token;
-        if(round%2){
-            token = 'X';
-            bgColor = warnaPemain2.fi;
-            fontColor = warnaPemain2.se;
-        } 
-        else{
-            token = 'O';
-            bgColor = warnaPemain1.fi;
-            fontColor = warnaPemain1.se;
-        }
-        
-        cout <<"(Pemain " << ((round%2) ? '1' : '2') << ") Pilih Kolom 1-7: ";
-        cekInput();
-        system("cls");
-        kotak[6-bawahKolom[kolom+1]+1][kolom+1] = token;
-        
-        //mengecek vertikal, horizontal, dan 2 diagonalnya:
-        if(connect4(6-bawahKolom[kolom+1]+1, kolom+1, token)){
-            cetakKotak();
-            cout << "Selamat Pemain " << ((round%2) ? 1 : 2)
-            << " Memenangkan Permainan dengan Connect Four " << 
-            arahConnectFour << endl;
-            
-            system("pause");
-            return 0;
-        }
-        
+    //MULAI RONDE PERMAINAN 
+    while(temp && skorPemain1 < ronde/2+1 && skorPemain2 < ronde/2+1){
+        bool imbang = true;
+        giliran = ((giliran==1) ? 2 : 1);
+        giliranTemp = giliran;
+        pergantianGiliran();
+       
         cout << bgColor << fontColor;
-        system("cls"); // membersihkan output kotak dan tulisan-tulisan sebelumnya
-        cetakKotak();
-        
-        
-        bawahKolom[kolom+1]++;
-        round++;
+        system("cls");
 
+        //set kotak kosong
+        for(int i = 1; i <= 7; i++){
+            for(int j = 1; j <= 9; j++){
+                if(j == 1 || j == 9){
+                    kotak[i][j] = '|';
+                }
+                else if(i==7){
+                    kotak[i][j] = '-';
+                }
+                else kotak[i][j] = ' ';
+                // cout << kotak[i][j] << " ";
+            }
+            // cout << endl;
+        }
+    
+        int round = 1;
+        fill(begin(bawahKolom), end(bawahKolom), 1);
+        cetakKotakBiasaDanSkor();
+        
+        while(round <= 42){
+            
+            cout <<"(" << ((giliranTemp==1) ? pemain1 : pemain2) << ", " << token << ") Pilih Kolom 1-7: ";
+            cekInput();
+            
+            cetakAnimasiKotak();
+
+            //mengecek vertikal, horizontal, dan 2 diagonalnya:
+            if(connect4(6-bawahKolom[kolom+1]+1, kolom+1, token)){
+                system("cls");
+                if(giliranTemp==1) skorPemain1++;
+                else skorPemain2++;
+                cout << "Ronde "<< ronde - temp + 1 << " (dari " << ronde << ")" << endl;
+                cout << "Skor " << pemain1 << ": " << skorPemain1 << endl;
+                cout << "Skor " << pemain2 << ": " << skorPemain2 << endl;
+                cout << endl;
+                cetakKotakKemenangan();
+                cout << endl;
+                cout << "Selamat " << ((giliranTemp==1) ? pemain1 : pemain2)
+                << " Memenangkan Permainan dengan Connect Four " << 
+                arahConnectFour << endl;
+
+                imbang = false;
+                break;
+            }
+            
+            giliranTemp = ((giliranTemp==1) ? 2 : 1);
+            pergantianGiliran(); // mengganti warna terminal dan token sesuai giliran pemain yang berganti
+            cout << bgColor << fontColor;
+            system("cls"); // membersihkan output kotak dan tulisan-tulisan sebelumnya
+            cetakKotakBiasaDanSkor();
+            
+            bawahKolom[kolom+1]++;
+            round++;
+        }
+    
+        if(imbang) cout << "Permainan Imbang, Silakan Coba Ulang Lagi Permainannya!" << endl;
+        else temp--;
+        if(temp){
+            cout << "Lanjutkan permainan? (y/n)";
+            string input;
+            while((cin >> input) && input != "y" && input != "n"){
+                cout << "Input tidak valid, silakan input kembali (y/n): ";
+            }
+            if(input == "n") break;
+        }
+        
     }
 
-    cout << "Permainan Imbang, Silakan Coba Ulang Lagi Permainannya!";
-    system("pause");
+    cout << endl;
+    if(skorPemain1 != skorPemain2) cout << "Selamat, " << ((skorPemain1 > skorPemain2) ? pemain1 : pemain2)
+    << " memenangkan permainan Connect Four ini dengan skor " << max(skorPemain1, skorPemain2) << " kemenangan dari" << ronde-temp << " ronde permainan" << endl;
+    else{
+        cout << "Permainan Connect Four ini berakhir imbang dengan skor masing-masing pemain: " << endl;
+        cout << pemain1 << ": " << skorPemain1 << endl;
+        cout << pemain2 << ": " << skorPemain2 << endl;
+    } 
+    
+    cout << endl;
+    cout << "Tutup terminal (y/n): ";
+    string terminal;
+    while((cin >> terminal) && terminal != "y"){
+        cout << "Tutup terminal (y/n): ";
+    }
     return 0;
 }
